b456eca5b4be5a8068e0a39d739ff4805a4902cf
toannguyen45 <nguyenminhtoan451999@gmail.com>
2025-01-25 22:14:40 +0700

    fix: fix layout auth

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,7 @@
 import React, { ReactNode } from "react";
-import { redirect } from "next/navigation";
 import Navbar from "../components/dashboard/Navbar";
 
 const DashboardLayout = async ({ children }: { children: ReactNode }) => {
-  // const session = await auth();
-
-  // if (!session) {
-  //   redirect("/admin/signin");
-  // }
-
   return (
     <>
       <Navbar />
