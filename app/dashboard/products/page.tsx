5184b5c468e28b4c0a546b91daaf94211998dc5c
toannguyen45 <nguyenminhtoan451999@gmail.com>
2025-02-07 01:04:46 +0700

    fix: fix product page

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,45 +1,9 @@
-import db from "@/lib/db";
-import React from "react";
-import { ProductClient } from "./components/client";
-import { ProductColumn } from "./components/columns";
-import { formatter } from "@/lib/utils";
-import { format } from "date-fns";
-import { unstable_noStore as noStore } from "next/cache";
+import React from 'react'
 
-const getData = async () => {
-  const data = await db.product.findMany({
-    include: {
-      category: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-
-  return data;
-};
-
-const ProductLists = async () => {
-  noStore();
-  const data = await getData();
-
-  const formattedProducts: ProductColumn[] = data.map((item) => ({
-    id: item.id,
-    name: item.name,
-    isFeatured: item.isFeatured,
-    isArchived: item.isArchived,
-    price: formatter.format(item.price.toNumber()),
-    category: item.category.name,
-    createdAt: format(item.createdAt, "MMM do, yyyy"),
-  }));
-
+const page = () => {
   return (
-    <div className="flex-col">
-      <div className="flex-1 space-y-4">
-        <ProductClient data={formattedProducts} />
-      </div>
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default ProductLists;
+export default page
