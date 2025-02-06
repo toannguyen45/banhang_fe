8b67b07fc6f8cad0d7ea6bc456b96652b78e75d7
toannguyen45 <nguyenminhtoan451999@gmail.com>
2025-02-07 00:48:13 +0700

    fix: fix productId

--- app/dashboard/products/[productId]/page.tsx
+++ app/dashboard/products/[productId]/page.tsx
@@ -1,23 +1,9 @@
-import db from "@/lib/db";
-import { ProductForm } from "./components/product-form";
+import React from 'react'
 
-export default async function ProductPage({
-  params,
-}: {
-  params: Promise<{ productId: string }>
-}) {
-  const { productId } = await params;
-
-  const product = await db.product.findUnique({
-    where: {
-      id: productId,
-    },
-    include: {
-      images: true,
-    },
-  });
-
-  const categories = await db.category.findMany({});
-
-  return <ProductForm initialData={product} categories={categories} />;
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
